12588db8cfcd6a83f046c4b8b0d2d896c4fedea8
ZRegistani <101371813+ZRegistani@users.noreply.github.com>
2025-12-30 21:33:02 -0500

    test: use example.com for stable CI UI test

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,12 +1,27 @@
 package tests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
+    protected WebDriver driver;
 
-    @Test
-    void sampleTest() {
-       System.out.println("CI is running JUnit 5 tests successfully");
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
 
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) driver.quit();
     }
 }
