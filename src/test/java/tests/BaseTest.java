7534dddcb6bb0d728eaa0ab054d68661d4bace7d
ZRegistani <101371813+ZRegistani@users.noreply.github.com>
2025-12-30 19:19:37 -0500

    test: add initial BaseTest for CI validation

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class BaseTest {
+
+    @Test
+    void sampleTest() {
+        System.out.println("CI is running tests successfully");
+    }
+}
