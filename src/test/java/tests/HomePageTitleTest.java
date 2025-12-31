00be7eb15431ed8212950f9ac9b7ae238a7ebb6a
ZRegistani <101371813+ZRegistani@users.noreply.github.com>
2025-12-31 13:28:06 -0500

    test: fix HomePageTitleTest compilation error

--- src/test/java/tests/HomePageTitleTest.java
+++ src/test/java/tests/HomePageTitleTest.java
@@ -7,4 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HomePageTitleTest extends BaseTest {
 
-    @
+    @Test
+    void homePageTitle_shouldBeExampleDomain() {
+        HomePage home = new HomePage(driver).open();
+        assertEquals("Example Domain", home.getTitle(), "Home page title should match");
+    }
+}
